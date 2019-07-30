4ac803aef9c313908aa5295d58da1afe162202ec
ninpucho <tbaker78@gmail.com>
2019-07-30 08:21:44 -0400

    Bonus Lab

---
+++ bonus/bonus01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Data List (Mixed Values)
+
+my_data = ["Pinta","Nina","Santa-Maria",3]
+
+
+#Print String
+
+print(f"Columbus had {my_data[3]} ships: the {my_data[1]}, the {my_data[0]}, and the {my_data[2]}.")
